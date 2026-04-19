b9d99e2300ed1d208d7b65693d4af6b3968525d7
Gazhib <bvalerianuly@gmail.com>
2026-04-20 00:22:03 +0500

    mailer

--- backend/mailer.js
+++ backend/mailer.js
@@ -2,7 +2,10 @@ const nodemailer = require("nodemailer");
 require("./loadEnv");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  requireTLS: true,
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_APP_PASSWORD,
